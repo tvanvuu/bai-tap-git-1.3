101dbb0eec1b2c5b78d5ce83d622554475eb783b
Tran Phuoc Hiep <tranphuochiep16012005@gmail.com>
2026-06-12 22:51:24 +0700

    fix: sửa lại đúng công thức tính gpa theo hệ số 4

--- main.py
+++ main.py
@@ -2,9 +2,4 @@ def tinh_diem_gpa(diem_so):
     if diem_so >= 8.5:
       return 4.0
     else:
-<<<<<<< HEAD
       return round((diem_so/10)*4,2) 
-=======
-      return ((diem_so/10)*3.5) 
-print("Điểm GPA hệ số 4 là: ", tinh_diem_gpa(8.5))
->>>>>>> 246f5c7e1940f6535a11ede1db79306e20545831
