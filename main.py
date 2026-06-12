246f5c7e1940f6535a11ede1db79306e20545831
Tran Phuoc Hiep <tranphuochiep16012005@gmail.com>
2026-06-12 22:27:40 +0700

    feat:Tối ưu lại công thức tính gpa

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ def tinh_diem_gpa(diem_so):
     if diem_so >= 8.5:
       return 4.0
     else:
-      return round((diem_so/10)*4,2) 
+      return ((diem_so/10)*3.5) 
+print("Điểm GPA hệ số 4 là: ", tinh_diem_gpa(8.5))
